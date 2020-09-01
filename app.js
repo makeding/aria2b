0869046b4726fa89dc2c57e8db44a566d8a07b16
huggy <6511667+makeding@users.noreply.github.com>
2020-09-01 10:12:12 +0800

    1.0.2 补充更新 1. 删掉一处 console.log (没错 我就是用这个 debug 的） 2. 增加个 try catch 这样不至于请求出错后 run_flag 依然为 false 然后整个脚本就炸了，就停止运行了

--- app.js
+++ app.js
@@ -10,38 +10,43 @@ let run_flag = true
 const secret = 'token:' + config.secret
 async function run() {
     run_flag = false
-    let d = await axios.post(base_url, {
-        jsonrpc: '2.0',
-        method: 'aria2.tellActive',
-        id: uuid(),
-        params: [secret, ['gid', 'status']]
-    })
-    await asyncForEach(d.data.result, async t => {
-        if (t.status == 'active') {
-            let d_peer = await axios.post(base_url, {
-                jsonrpc: '2.0',
-                method: 'system.multicall',
-                id: uuid(),
-                params: [[{ 'methodName': 'aria2.getPeers', 'params': [secret, t.gid] }]]
-            })
-            await asyncForEach(d_peer.data.result[0][0], peer => {
-                let c = get_peer_name(decodePercentEncodedString(peer.peerId))
-                console.log(c)
-                if (blocked_ips.indexOf(peer.ip) == -1) {
-                    if (config.block_keywords.indexOf('Unknow') > -1 && c.client == 'unknown') {
-                        block_ip(peer.ip, {
-                            origin: 'unknow',
-                            client: '',
-                            version: ''
-                        })
-                    } else if (new RegExp('(' + config.block_keywords.join('|') + ')').test(c.origin)) {
-                        block_ip(peer.ip, c)
+    try {
+        let d = await axios.post(base_url, {
+            jsonrpc: '2.0',
+            method: 'aria2.tellActive',
+            id: uuid(),
+            params: [secret, ['gid', 'status']]
+        })
+        await asyncForEach(d.data.result, async t => {
+            if (t.status == 'active') {
+                let d_peer = await axios.post(base_url, {
+                    jsonrpc: '2.0',
+                    method: 'system.multicall',
+                    id: uuid(),
+                    params: [[{ 'methodName': 'aria2.getPeers', 'params': [secret, t.gid] }]]
+                })
+                await asyncForEach(d_peer.data.result[0][0], peer => {
+                    let c = get_peer_name(decodePercentEncodedString(peer.peerId))
+                    if (blocked_ips.indexOf(peer.ip) == -1) {
+                        if (config.block_keywords.indexOf('Unknow') > -1 && c.client == 'unknown') {
+                            block_ip(peer.ip, {
+                                origin: 'unknow',
+                                client: '',
+                                version: ''
+                            })
+                        } else if (new RegExp('(' + config.block_keywords.join('|') + ')').test(c.origin)) {
+                            block_ip(peer.ip, c)
+                        }
                     }
-                }
-            })
-        }
+                })
+            }
+            run_flag = true
+        })
+    } catch (e) {
         run_flag = true
-    })
+        console.error('请求错误 日志如下，请检查是否填错 url 和 secret，或者 aria2 进程嗝屁了')
+        console.error(e)
+    }
 
 }
 setInterval(() => {
